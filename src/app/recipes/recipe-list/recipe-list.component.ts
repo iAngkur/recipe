6d6071f26b95cc528d95cd16a53e6276972a48b8
Angkur Mondal <angkurmondal1998@gmail.com>
2024-07-31 16:56:22 +0600

    added action buttons in all components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 
 @Component({
@@ -7,5 +8,9 @@ import {Component } from "@angular/core";
     styleUrl: "./recipe-list.component.css"
 })
 export class RecipeListComponent {
+
+    recipes: Recipe[] = [
+        new Recipe("A Test Recipe", "This is simply a test", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZxLCcgAEowoTnf_65Kvxa708Ko771XUjgDw&s")
+    ];
 
 }
